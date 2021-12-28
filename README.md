8069ee22143cdc2b96b33d58fe4b9dd8095c7f00
= <idkman619@gmail.com>
2021-12-28 11:53:05 -0500

    Updated Apache Camel Support

---
+++ README.md
@@ -0,0 +1,6 @@
+Spring Boot Application for Property Management
+
+To Run;
+
+java -jar app.jar --pathToCsvFile="/path/to/csv/file.csv"
+
--- src/test/java/com/bern/norstienpaymentsapi/NorstienpaymentsapiApplicationTests.java
+++ src/test/java/com/bern/norstienpaymentsapi/NorstienpaymentsapiApplicationTests.java
@@ -2,8 +2,10 @@ package com.bern.norstienpaymentsapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "pathToCsvFile=/home/unknown/Downloads/propertyApi.csv")
 class NorstienpaymentsapiApplicationTests {
 
 	@Test
